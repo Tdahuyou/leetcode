5ca8fbb2e66e0dba7c1a8ef2345a745bbefbe4e0
Tdahuyou <12875288+tdahuyou@user.noreply.gitee.com>
2025-01-19 18:51:14 +0800

    update

--- 0001. 两数之和【简单】/题解/3/1.ts
+++ 0001. 两数之和【简单】/题解/3/1.ts
@@ -1,11 +1,12 @@
-var twoSum = function (nums, target) {
-  const map = new Map();
+function twoSum(nums: number[], target: number): number[] {
+  const map: Map<number, number> = new Map();
   for (let i = 0; i < nums.length; i++) {
     const item = nums[i];
     const anotherNum = target - item;
     if (map.has(anotherNum)) {
-      return [i, map.get(anotherNum)];
+      return [i, map.get(anotherNum)!];
     }
     map.set(item, i);
   }
-};
+  return [];
+}
